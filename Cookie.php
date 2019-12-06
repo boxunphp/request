0d3b4f9c7e535f76b7f78dd2c263ac22984c5161
Jordy <arno.zheng@gmail.com>
2019-12-06 14:01:19 +0800

    init: Request Class

---
+++ Cookie.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordy
+ * Date: 2019/12/6
+ * Time: 1:48 PM
+ */
+
+namespace All\Request;
+
+class Cookie
+{
+    public function get($key, $default = null, $prefix = '')
+    {
+        $key = $prefix . $key;
+        return isset($_COOKIE[$key]) ? $_COOKIE[$key] : $default;
+    }
+
+    public function set($name, $value, $lifetime = 0)
+    {
+        $params = session_get_cookie_params();
+        $currTime = time();
+        $lifetime = $lifetime > 0 ? $currTime + $lifetime : ($params['lifetime'] ? $currTime + $params['lifetime'] : 0);
+        return setcookie($name, $value, $lifetime, $params['path'], $params['domain'], $params['secure'],
+            $params['httponly']);
+    }
+}
---
+++ tests/RequestTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordy
+ * Date: 2019/12/6
+ * Time: 1:59 PM
+ */
+
+namespace RequestTest;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+
+}
